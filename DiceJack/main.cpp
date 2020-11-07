cec1771568b798681ade4652424940d8852f2115
clalil <cliljander@gmail.com>
2020-11-07 11:24:59 +0100

    Initial Commit

---
+++ DiceJack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DiceJack
+//
+//  Created by Clarissa Liljander on 2020-11-07.
+//  Copyright © 2020 Clarissa Liljander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
