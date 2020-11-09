24bbd4a252abae53d8895a456972ec16a67369d9
clalil <cliljander@gmail.com>
2020-11-09 18:46:04 +0100

    WIP remodel code, first part done

--- DiceJack/main.cpp
+++ DiceJack/main.cpp
@@ -6,13 +6,15 @@
 //  Copyright © 2020 Clarissa Liljander. All rights reserved.
 //
 #include <random>
-#include <vector>        //vector
+#include <vector>
 #include <iostream>
 using namespace std;
 
 // Set up our random device and number generator as globals
-std::random_device randomDevice;
-std::mt19937 randomGenerator = std::mt19937(randomDevice());
+mt19937 random_generator = mt19937(0);
+uniform_int_distribution<int> dice_roller(1, 6);
+int credits = 100;
+int input = 0;
 
 void greeting() {
     cout << "Welcome to Dicejack! The rules are simple:" << "\n";
@@ -26,54 +28,65 @@ void current_credits(int credits) {
     cout << "[2] Leave the table." << "\n";
 }
 
-void current_total(vector<int> dice_rolled, int total) {
-    cout << "I have rolled 2 six-sided dice and they came up as: " << "\n";
-    cout << dice_rolled[0] << " and " << dice_rolled[1] << "." << "\n";
-    cout << "Your total is currently " << total << ". You will go bust if you score over 21." << "\n";
+void current_total(int player_total) {
+    cout << "Your total is currently " << player_total << "." << "\n";
+    cout << "You will go bust if you score over 21." << "\n";
+    cout << "What would you like to do next?" << "\n";
+    cout << "[1] Stick" << "\n";
+    cout << "[2] Roll again" << "\n";
 }
 
-// Create a random integer function that uses the global random generator
-int randomInteger(int min, int max) {
-    std::uniform_int_distribution<int> diceRoller(min, max);
-    return diceRoller(randomGenerator);
+int random_dice(void) {
+    return dice_roller(random_generator);
+}
+
+void play_round() {
+    int bet = 0;
+    int player_total = 0;
+    bool playing = true;
+    
+    cout << "Please place your bet (maximum 50)?" << "\n";
+    cin  >> bet;
+
+    if (bet > 50) {
+        return;
+    }
+
+    while (playing) {
+        random_dice();
+        
+        for(int i = 0; i < 2; ++i) {
+            player_total += dice_roller(random_generator);
+        }
+
+        if (player_total > 21) {
+            cout << "You went bust!" << "\n";
+            credits -= bet;
+            return;
+        }
+        
+        current_total(player_total);
+        cin >> input;
+        
+        if (input == 1) {
+            
+        } else if (input == 2) {
+            
+        }
+    }
 }
 
 int main() {
-    vector<int> dice_rolled = {};
     bool gameover = false;
-    int credits = 100;
-    int input = 0;
-    int bet = 0;
-    int total = 0;
     
     greeting();
     
     while(gameover != true && credits > 0) {
         current_credits(credits);
-        
         cin >> input;
         
-        if(input == 1) {
-            cout <<
-            "How much would you like to bet (maximum 50)?" << "\n";
-            cin >> bet;
-
-            if(bet > 50) {
-                cout << "Your bet is too high. Bet again." << "\n";
-                cin >> bet;
-            } else {
-                for(int i = 0; i < 2; ++i) {
-                    total += randomInteger(1, 6);
-                    dice_rolled.push_back(total);
-                }
-                
-                current_total(dice_rolled, total);
-                
-                cout << "What would you like to do next?" << "\n";
-                cout << "[1] Stick" << "\n";
-                cout << "[2] Roll again" << "\n";
-            }
-
+        if (input == 1) {
+            play_round();
         } else if (input == 2) {
             gameover = true;
         }
